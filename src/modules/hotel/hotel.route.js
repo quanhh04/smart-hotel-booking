4c58327e6d891067cc88f66a955048c1a520adfa
tu.ta <tu.ta@lpbs.com.vn>
2026-04-12 13:16:15 +0700

    validate

--- src/modules/hotel/hotel.route.js
+++ src/modules/hotel/hotel.route.js
@@ -2,16 +2,15 @@ const { Router } = require('express');
 const hotelController = require('./hotel.controller');
 const authMiddleware = require('../../common/middleware/auth.middleware');
 const requireAdmin = require('../../common/middleware/require-admin');
-const validate = require('../../common/middleware/validate');
-const hotelSchemas = require('./hotel.schema');
+const { validateGetHotels, validateCreateHotel, validateGetHotelDetail, validateUpdateHotel, validateDeleteHotel, validateGetHotelRooms } = require('./hotel.validate');
 
 const router = Router();
 
-router.get('/', validate(hotelSchemas.getHotels), hotelController.getHotels);
-router.post('/', authMiddleware, requireAdmin, validate(hotelSchemas.createHotel), hotelController.createHotel);
-router.get('/:id/rooms', validate(hotelSchemas.getHotelRooms), hotelController.getHotelRooms);
-router.get('/:id', validate(hotelSchemas.getHotelDetail), hotelController.getHotelDetail);
-router.put('/:id', authMiddleware, requireAdmin, validate(hotelSchemas.updateHotel), hotelController.updateHotel);
-router.delete('/:id', authMiddleware, requireAdmin, validate(hotelSchemas.deleteHotel), hotelController.deleteHotel);
+router.get('/', validateGetHotels, hotelController.getHotels);
+router.post('/', authMiddleware, requireAdmin, validateCreateHotel, hotelController.createHotel);
+router.get('/:id/rooms', validateGetHotelRooms, hotelController.getHotelRooms);
+router.get('/:id', validateGetHotelDetail, hotelController.getHotelDetail);
+router.put('/:id', authMiddleware, requireAdmin, validateUpdateHotel, hotelController.updateHotel);
+router.delete('/:id', authMiddleware, requireAdmin, validateDeleteHotel, hotelController.deleteHotel);
 
 module.exports = router;
